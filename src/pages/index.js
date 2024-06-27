6f431c20020d4292c5325469d3ff8db5833ac8b4
Danny Rios <riosdcs@gmail.com>
2024-06-27 16:05:17 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World.{" "}
+      Hello Preview.{" "}
       <Link href="/about">
         About
       </Link>
